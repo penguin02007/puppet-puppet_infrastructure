39ee4c4f0aad6a380e291e56d9119adb7ae66239
Peter Pouliot <peter@pouliot.net>
2017-11-03 14:05:04 -0400

    updating bootstrap.sh

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -23,4 +23,4 @@ echo "### INSTALLING R10K and Hierra-eyaml ###"
 gem install r10k hiera-eyaml
 cd /etc/puppetlabs/code && wget https://raw.githubusercontent.com/ppouliot/puppet-puppet_infrastructure/master/Puppetfile
 /usr/local/bin/r10k puppetfile install --verbose DEBUG2
-/opt/puppet/bin/puppet apply --debug -trace --verbose --modulepath=/etc/puppetlabs/code/modules /etc/puppetlabs/code/manifests
+/opt/puppet/bin/puppet apply --debug -trace --verbose --modulepath=/etc/puppetlabs/code/modules /etc/puppetlabs/code/modules/puppet_infrasturcture/examples/init.pp
