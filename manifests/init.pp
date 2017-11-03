58b667e874029be21defaaeca382c924fb9107fd
Peter Pouliot <peter@pouliot.net>
2017-11-03 15:18:10 -0400

    changing order for puppetboard

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,9 +11,9 @@ class puppet_infrastructure{
   class{'puppet_infrastructure::puppetserver_master':}
   class{'puppet_infrastructure::config':}
   class{'puppet_infrastructure::puppet_mgmt_scripts':}
-  class{'puppet_infrastructure::puppetboard_server':}
   class{'puppet_infrastructure::puppetdb_server':}
   class{'puppet_infrastructure::puppetserver_connect_puppetdb':}
+  class{'puppet_infrastructure::puppetboard_server':}
   class{'puppet_infrastructure::puppetexplorer_vhost':}
 
   contain 'puppet_infrastructure::puppetserver_master'
@@ -29,7 +29,7 @@ class puppet_infrastructure{
   -> Class['puppet_infrastructure::config']
     -> Class['puppet_infrastructure::puppet_mgmt_scripts']
       -> Class['puppet_infrastructure::puppetdb_server']
-        -> Class['puppet_infrastructure::puppetboard_server']
-          -> Class['puppet_infrastructure::puppetserver_connect_puppetdb']
+        -> Class['puppet_infrastructure::puppetserver_connect_puppetdb']
+          -> Class['puppet_infrastructure::puppetboard_server']
             -> Class['puppet_infrastructure::puppetexplorer_vhost']
 }
