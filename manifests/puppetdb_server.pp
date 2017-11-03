83506ef6d21aac39c77b08e9b9c49203ee21b44c
Peter Pouliot <peter@pouliot.net>
2017-11-03 15:59:41 -0400

    adding cert gen

--- manifests/puppetdb_server.pp
+++ manifests/puppetdb_server.pp
@@ -9,11 +9,13 @@
 class puppet_infrastructure::puppetdb_server {
 
   class { 'puppetdb':
-    listen_address => '0.0.0.0',
-    node_ttl       => '2d',
-    node_purge_ttl => '3d',
-    report_ttl     => '3d',
-    java_args      => {
+    ssl_set_cert_paths => true,
+    ssl_deploy_certs   => true,
+    listen_address     => '0.0.0.0',
+    node_ttl           => '2d',
+    node_purge_ttl     => '3d',
+    report_ttl         => '3d',
+    java_args          => {
       '-Xmx' => '1g',
     }
   }
