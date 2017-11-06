0e12132a64a06adccd6ffb43991df56d2ddd781f
Peter Pouliot <peter@pouliot.net>
2017-11-06 09:27:57 -0500

    attempting to get everything installed

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,24 +12,24 @@ class puppet_infrastructure{
   class{'puppet_infrastructure::config':}
   class{'puppet_infrastructure::puppet_mgmt_scripts':}
   class{'puppet_infrastructure::puppetdb_server':}
-#  class{'puppet_infrastructure::puppetserver_connect_puppetdb':}
-#  class{'puppet_infrastructure::puppetboard_server':}
-#  class{'puppet_infrastructure::puppetexplorer_vhost':}
+  class{'puppet_infrastructure::puppetserver_connect_puppetdb':}
+  class{'puppet_infrastructure::puppetboard_server':}
+  class{'puppet_infrastructure::puppetexplorer_vhost':}
 
   contain 'puppet_infrastructure::puppetserver_master'
   contain 'puppet_infrastructure::config'
   contain 'puppet_infrastructure::puppetdb_server'
   contain 'puppet_infrastructure::puppet_mgmt_scripts'
 #  contain 'puppet_infrastructure::os_gems'
-#  contain 'puppet_infrastructure::puppetboard_server'
-#  contain 'puppet_infrastructure::puppetserver_connect_puppetdb'
-#  contain 'puppet_infrastructure::puppetexplorer_vhost'
+  contain 'puppet_infrastructure::puppetboard_server'
+  contain 'puppet_infrastructure::puppetserver_connect_puppetdb'
+  contain 'puppet_infrastructure::puppetexplorer_vhost'
 
   Class['puppet_infrastructure::puppetserver_master']
   -> Class['puppet_infrastructure::config']
     -> Class['puppet_infrastructure::puppet_mgmt_scripts']
       -> Class['puppet_infrastructure::puppetdb_server']
-#          -> Class['puppet_infrastructure::puppetboard_server']
-#            -> Class['puppet_infrastructure::puppetexplorer_vhost']
-#              -> Class['puppet_infrastructure::puppetserver_connect_puppetdb']
+          -> Class['puppet_infrastructure::puppetboard_server']
+            -> Class['puppet_infrastructure::puppetexplorer_vhost']
+              -> Class['puppet_infrastructure::puppetserver_connect_puppetdb']
 }
